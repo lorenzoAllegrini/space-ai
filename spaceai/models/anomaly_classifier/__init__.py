3f2408044448757172b8f53d7e0f22d1bbcfecef
lorenzoAllegrini <l.allegrini1@studenti.unipi.it>
2025-12-01 17:55:08 +0100

    fix run_exp

--- spaceai/models/anomaly_classifier/__init__.py
+++ spaceai/models/anomaly_classifier/__init__.py
@@ -1,12 +1,10 @@
 from .anomaly_classifier import AnomalyClassifier
 from .dpmm_detector import DPMMDetector
 from .rockad import RockadClassifier, NearestNeighborOCC
-from .rocket import RocketClassifier
 
 __all__ = [
     "AnomalyClassifier",
     "DPMMDetector",
     "RockadClassifier",
     "NearestNeighborOCC",
-    "RocketClassifier",
 ]
